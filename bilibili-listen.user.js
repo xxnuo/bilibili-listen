d4c859dcb03945e2956f519b9dce2952200718f3
xxnuo <bigtear@vip.qq.com>
2024-05-21 23:19:36 +0800

    Update bilibili-listen.user.js

--- bilibili-listen.user.js
+++ bilibili-listen.user.js
@@ -2,94 +2,155 @@
 // @name         Bilibili 音频模式 [听视频]
 // @namespace    xxnuo
 // @version      1.0.0
-// @description  给播放器添加一个音频播放条
+// @description  给播放器上方添加一个音频播放条
 // @author       xxnuo
 // @match        https://www.bilibili.com/video/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
 // @grant        unsafeWindow
+// @grant        GM_xmlhttpRequest
+// @grant        GM_addElement
+// @run-at       document-start
+// @require      https://greasyfork.org/scripts/449444-hook-vue3-app/code/Hook%20Vue3%20app.js
+// @require      https://cdn.jsdelivr.net/npm/aplayer@1.10.1/dist/APlayer.min.js
+// @connect      api.bilibili.com
 // @license      MIT
 // ==/UserScript==
 
 (function () {
   "use strict";
+  const quality = "max"; // or min
+  const isHideVideo = false;
+  const isHideComment = false;
   const log = (...args) => console.log("[LISTEN] ", ...args);
   const win = typeof unsafeWindow === "undefined" ? window : unsafeWindow;
+  const get = (obj, paths) => paths.reduce((prev, path) => (typeof prev === "undefined" ? undefined : prev[path]), obj);
+  const playerWarpEle = document.getElementById("playerWrap");
+
   log("START");
 
-  //   // 通过播放器获取信息，不使用
-  //   const get = (obj, paths) => paths.reduce((prev, path) => (typeof prev === "undefined" ? undefined : prev[path]), obj);
+  window._vueUnhooked_ = vueUnhooked;
+  window._vueHooked_ = vueHooked;
 
-  //   const curP = get(win, ["__INITIAL_STATE__", "p"]);
-  //   const totalP = get(win, ["__INITIAL_STATE__", "videoData", "videos"]);
-  //   const cid = get(win, ["__INITIAL_STATE__", "videoData", "cid"]);
-  //   const pic = get(win, ["__INITIAL_STATE__", "videoData", "pic"]);
+  log(playerWarpEle);
 
-  // 直接将播放器干掉并换为音频播放器，省流省资源
+  const hideBiliPlayer = () => {
+    if (isHideVideo) {
+      if (playerWarpEle) {
+        //   log("process");
+        // playerWarpEle.style.display = "none";
 
-  let playerWarpEle = document.getElementById("playerWrap");
-  if (playerWarpEle) {
-    // 隐藏播放器
-    let bilibiliPlayerEle = playerWarpEle.querySelector("#bilibili-player");
-    let bilibiliPlayerPlaceholderEle = playerWarpEle.querySelector("#bilibili-player-placeholder");
-    if (bilibiliPlayerEle) {
-        playerWarpEle.removeChild(bilibiliPlayerEle);
-      // bilibiliPlayerEle.style.display = "none";
+        const bilibiliPlayerEle = playerWarpEle.querySelector("#bilibili-player");
+        const bilibiliPlayerPlaceholderEle = playerWarpEle.querySelector("#bilibili-player-placeholder");
+        if (bilibiliPlayerEle) {
+          bilibiliPlayerEle.style.display = "none";
+        }
+        if (bilibiliPlayerPlaceholderEle) {
+          bilibiliPlayerPlaceholderEle.style.display = "none";
+          const top = bilibiliPlayerPlaceholderEle.querySelector("#bilibili-player-placeholder-top");
+          const bot = bilibiliPlayerPlaceholderEle.querySelector("#bilibili-player-placeholder-bottom");
+          top.style.display = "none";
+          bot.style.display = "none";
+        }
+      }
     }
-    if (bilibiliPlayerPlaceholderEle) {
-        // playerWarpEle.removeChild(bilibiliPlayerPlaceholderEle);
-      bilibiliPlayerPlaceholderEle.style.display = "none";
+  };
+  hideBiliPlayer();
+
+  const hideComment = () => {
+    if (isHideComment) {
+      const commentEle = document.getElementById("comment");
+      if (commentEle) {
+        commentEle.style.display = "none";
+        // commentEle.remove();
+      }
+    }
+  };
+  hideComment();
+
+  let originalPushState = history.pushState;
+  history.pushState = function (state) {
+    let result = originalPushState.apply(this, arguments);
+    let event = new Event("pushstate");
+    event.state = state;
+    window.dispatchEvent(event);
+    return result;
+  };
+
+  GM_addElement("link", {
+    rel: "stylesheet",
+    href: "https://cdn.jsdelivr.net/npm/aplayer@1.10.1/dist/APlayer.min.css",
+  });
+
+  const aplayerEle = GM_addElement("div", {
+    id: "aplayer",
+  });
+  playerWarpEle.parentElement.insertBefore(aplayerEle, playerWarpEle);
+
+  const listen_aplayer = new APlayer({
+    container: document.getElementById("aplayer"),
+    // fixed: true,
+  });
+
+  const reloadPlayer = () => {
+    const curP = get(win, ["__INITIAL_STATE__", "p"]);
+    const bvID = get(win, ["__INITIAL_STATE__", "bvid"]);
+    const cid = get(win, ["__INITIAL_STATE__", "cidMap", bvID, "cids", curP]);
+
+    const pic = get(win, ["__INITIAL_STATE__", "videoData", "pic"]);
+    const author = get(win, ["__INITIAL_STATE__", "videoData", "owner", "name"]);
+    const title = get(win, ["__INITIAL_STATE__", "videoData", "title"]);
+
+    if (isHideVideo) {
+      const coverEle = GM_addElement("img", {
+        id: "listen-cover",
+        src: pic,
+        alt: title,
+        style: `object-fit: scale-down;`,
+      });
+      playerWarpEle.parentElement.insertBefore(coverEle, playerWarpEle);
     }
 
-    let coverUrl = "";
-    let cID = "";
+    const url = `https://api.bilibili.com/x/player/wbi/playurl?bvid=${bvID}&cid=${cid}&fnval=16`;
+    // log("fetch url", url);
+    GM_xmlhttpRequest({
+      method: "GET",
+      url: url,
+      cookie: true,
+      onload: function (response) {
+        var res = JSON.parse(response.responseText).data;
+        var url;
 
-    // const metaUrl = document.querySelector('meta[itemprop="url"]');
-    const metaUrl = document.querySelector('link[rel="canonical"]');
-    const metaImage = document.querySelector('meta[itemprop="image"]');
+        if (res.dash) {
+          var audios = res.dash.audio;
 
-    if (metaImage) {
-      // 获取并格式化封面尺寸
-      coverUrl = metaImage.getAttribute("content").split("@")[0];
-    } else {
-      console.log("LISTEN:Video Cover Image not found on the page.");
-      coverUrl = "https://i2.hdslb.com/bfs/space/768cc4fd97618cf589d23c2711a1d1a729f42235.png";
-    }
+          if (quality === "max") {
+            audios.sort((a, b) => a.bandwidth - b.bandwidth);
+          } else if (quality === "min") {
+            audios.sort((a, b) => b.bandwidth - a.bandwidth);
+          }
+          url = audios[0].baseUrl;
+        } else {
+          url = res.durl[0].url;
+        }
 
-    // let coverEle = document.createElement("img");
-    // coverEle.id = "bilibili-listen-cover";
-    // coverEle.src = coverUrl;
-    // coverEle.style.width = "100%";
-    // coverEle.style.height = "100%";
-    // coverEle.style.objectFit = "contain";
+        // log("audio url ", url);
 
-    // playerWarpEle.appendChild(coverEle);
-
-    let coverHtml = `<img id="bilibili-listen-cover" src="${coverUrl}" style="width: 100%; height: 100%; object-fit: contain;">`;
-    playerWarpEle.insertAdjacentHTML("beforeend", coverHtml);
-
-    console.log("LISTEN:metaUrl:", metaUrl);
-    if (metaUrl) {
-      let videoUrlParts = metaUrl.getAttribute("href").split("/").reverse();
-      let bvIDwithP = videoUrlParts.find((part) => part.startsWith("BV")).split("?");
-      let bvID = bvIDwithP[0];
-      console.log("LISTEN:bvID:", bvID);
-      let p = 0;
-      if (bvIDwithP[1]) p = bvIDwithP[1];
-      // 通过 bvID 获取 cID，接口 //api.bilibili.com/x/player/pagelist?bvid=${bvID} 返回值内 json.data[0].cid
-
-      fetch(`https://api.bilibili.com/x/player/pagelist?bvid=${bvID}`)
-        .then((response) => response.json())
-        .then((data) => {
-          cID = data.data[p].cid;
-          console.log("LISTEN:cID:", cID);
+        listen_aplayer.list.clear();
+        listen_aplayer.list.add({
+          name: title,
+          artist: author,
+          url: url,
+          cover: pic,
         });
-    } else {
-      console.log("LISTEN:Video ID not found on the page.");
-      return;
-    }
-  } else {
-    // 未找到播放器元素
-    console.log("LISTEN:Player element not found on the page.");
-    return;
-  }
+
+        console.log("[LISTEN] Bilibili 音频模式 [听视频] 已加载");
+      },
+    });
+  };
+
+  reloadPlayer();
+
+  win.addEventListener("pushstate", reloadPlayer);
+
+  log("END");
 })();
